34fc4e4898e3e9e33e34e3b361d56aa4496db9b9
Frederico Cassola <cfcassola@gmail.com>
2023-06-12 10:03:31 +0200

    Add verify token

--- app.js
+++ app.js
@@ -14,25 +14,30 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
+
+const { hashPassword, verifyPassword, verifyToken } = require("./auth.js");
+const { getUserByEmailWithPasswordAndPassToNext } = require("./userHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+app.post("/api/users", hashPassword, userHandlers.postUser);
+app.post("/api/login", getUserByEmailWithPasswordAndPassToNext, verifyPassword);
+
+app.use(verifyToken);
+
 app.post("/api/movies", movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
-const userHandlers = require("./userHandlers");
-
-app.get("/api/users", userHandlers.getUsers);
-app.get("/api/users/:id", userHandlers.getUserById);
-// app.post("/api/users", userHandlers.postUser);
-// app.put("/api/users/:id", userHandlers.updateUser);
+app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
 
-const { hashPassword } = require("./auth.js");
 
-app.post("/api/users", hashPassword, userHandlers.postUser);
-app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
 
 app.listen(port, (err) => {
   if (err) {
